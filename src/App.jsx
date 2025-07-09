14ddd365c5f431d7537c4fb9da981fe4937b27f7
Bereket-Eshete <bereketeshete63@gmail.com>
2025-07-09 18:26:06 +0300

    add useEffect hook to make request

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import Header from "./components/Header";
-
+import Meme from "./components/meme";
 const App = () => {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Meme />;
+    </div>
+  );
 };
 export default App;
